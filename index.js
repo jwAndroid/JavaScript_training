69965c0bbf1a08b1eb97217c8a9f2e1af7c8f57a
jwAndroid <cjd9408er@naver.com>
2021-07-27 20:16:54 +0900

    0727 commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// console.log("helloWorld")
+//var 를 쓰면 안되는 이유 (절떄)
+//호이 스팅 ? : 자바스크립트에서 var 를 쓰면 안되는 이유 좀더 궁금하면 구글링 하여튼
+
+//let 변수 const 상수 단 , const가 95% 쓴다. 거의 let 변수를 쓸 일이 없다.
+//콘솔창 화살표 위로하면 바로 런할수있을
+//자료형 생략 , 그냥 숫자 쓰면 됨
+
+// const a = 1
+// console.log(a)
+
+// const b = 2
+// console.log(b)
+
+// const a = "hello";
+// console.log(a);
+
+// const isLoading = true;
+// console.log(isLoading);
+
+// const friend = null;
+// console.log(friend);
+// // friend null , 
+
+// let criminal;
+// console.log(criminal);
+// // 초기화하지 않은 선언은 let을 쓴다.
+
+// const a = 1+2;
+// console.log(a);
+
+// const b = 1+2 - (3*4)/4;
+// console.log(b);
+
+// let c  = 1;
+// console.log(c++);
+// console.log(++c);
+
+// let d = 1;
+// d += 3;
+// console.log(d);
+
+// d -= 3;
+// console.log(d);
+
+// d *= 3;
+// console.log(d);
+
+// d /= 3;
+// console.log(d);
+
+
+// const a = !true;
+// console.log(a)
+
+// const b = true && true;
+// console.log(b);
+
+// // const c = false && true;
+// // console.log(c);
+
+// const c = false || false;
+// console.log(c)
+
+// const d = false || true;
+// console.log(d)
+
+// const e = !((true && false) || (true && false) || !false);
+// console.log(e)
+
+// == << 두개 쓰지말고 === 세게 쓸것 
+
+// const a = 1 ;
+// const b = 1 ;
+// console.log(a === b);
+// console.log(a !== b);
+
+// const c = 10;
+// const d = 15;
+// const e = 15;
+
+// console.log(c < d) 
+// console.log(c > d) 
+// console.log(d >= e) 
+// console.log(c <= e) 
+//구분할때 변수를 앞에두기
