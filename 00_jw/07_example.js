0b567099f599f9b0392ded9a285fa744fa89fda9
jwAndroid <cjd9408er@naver.com>
2022-08-22 23:51:47 +0900

    remove unnotation

--- 00_jw/07_example.js
+++ 00_jw/07_example.js
@@ -7,20 +7,11 @@ const biggerThanThree = (arrays) => {
   }, 0);
   return console.log(addValue);
 };
-// map은 이렇게 쓰면안된다.
-// map 이라는게 map 뒤에서 연산을해서 const result 에 값을 받아와야지
 
 const biggerThanThree2 = (arrays) => {
   const result = arrays.reduce((sum, arr) => sum + arr, 0);
   console.log(result);
   return result;
-
-  // let result = 0;
-  // const result = arrays.map((array) => {
-  //   result += array;
-  // });
-  // map 으로는 배열이 나오니까 안된다.
-  // console.log(result);
 };
 
 // arr.push(2) === 자체는 넣은상태의 length 를 리턴함.
